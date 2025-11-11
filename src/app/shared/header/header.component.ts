b6dbac8ab996ba1101708e79cc9ff0ba649fca76
Virysrm <virysrmz@gmail.com>
2025-11-11 17:15:36 -0600

    Modificaciones y ajustes a menu y contacto para moviles

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
     standalone: false
 })
 export class HeaderComponent implements OnInit {
-
+menuOpen = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+toggleMenu() {
+  this.menuOpen = !this.menuOpen;
+  document.body.style.overflow = this.menuOpen ? 'hidden' : ''; // evita scroll
+}
 
 }
