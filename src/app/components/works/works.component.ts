35d24fde4df2962c043f015c7316777e6cc44810
Virysrm <virysrmz@gmail.com>
2026-04-22 20:59:52 -0600

    Modificaciones a animaciones y estilos de careba

--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -10,25 +10,42 @@ export class WorksComponent implements AfterViewInit {
   @ViewChild('fadeEl') fadeEl!: ElementRef;
   isFadeVisible = false;
 
+  activeIndex: number = -1;
+
     cards = [
     { bg:'#434448', icon:'bi bi-pencil-square', title:'Diseño Personalizado', text:'Cada pieza es única, diseñada específicamente para tu espacio y necesidades', isTouched: false },
     { bg:'#1e3a47', icon:'bi bi-rulers', title:'Muebles a Medida', text:'Diseñamos y fabricamos muebles únicos adaptados a tus necesidades y espacio.', isTouched: false },
     { bg:'#4b2848', icon:'bi bi-stars', title:'Restauración Premium', text:'Devolvemos la vida a tus muebles con excelentes técnicas de restauracion.', isTouched: false },
-    { bg:'#1d442d', icon:'bi bi-hammer', title:'Carpintería Integral', text:'Desde armarios hasta terrazas, todo tipo de proyectos en madera tanto interior como exterior', isTouched: false }
+    { bg:'#1d442d', icon:'bi bi-hammer', title:'Carpintería Integral', text:'Desde armarios hasta terrazas, en madera para interior y exterior', isTouched: false }
   ];
 
-  ngAfterViewInit() {
-    const fadeObserver = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          this.isFadeVisible = true;
-          fadeObserver.unobserve(this.fadeEl.nativeElement); // solo una vez
-        }
-      },
-      { threshold: 0.2 }
-    );
+animateCardsSequentially() {
+  this.cards.forEach((card, index) => {
+    
+    // Primero: hacer visible la card
+    setTimeout(() => {
+      card.isTouched = true;
+    }, index * 500);
 
-    if (this.fadeEl) fadeObserver.observe(this.fadeEl.nativeElement);
-  }
+    // Segundo: mover el borde activo
+    setTimeout(() => {
+      this.activeIndex = index;
+    }, index * 800); // 👈 este delay es clave
+  });
+}
+
+  ngAfterViewInit() {
+  const fadeObserver = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        this.animateCardsSequentially();
+        fadeObserver.unobserve(this.fadeEl.nativeElement);
+      }
+    },
+    { threshold: 0.2 }
+  );
+
+  if (this.fadeEl) fadeObserver.observe(this.fadeEl.nativeElement);
+}
   
 }
